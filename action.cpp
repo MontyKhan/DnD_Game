716a88b669afa88010077639ff270c010764af1e
MontyKhan <robclark.wgc@gmail.com>
2020-06-30 22:50:24 +0100

    Split action main() into smaller functions.

--- action.cpp
+++ action.cpp
@@ -33,6 +33,41 @@ int roll_dice(int num, int dice, int mod)
 	return damage + mod;
 }
 
+int take_action(std::vector<std::string> creature)
+{
+	int attack_mod = stoi(creature[ATTACK_VAR]);
+	cout << "Attack: " << roll_dice(1,20,attack_mod) << endl;	// Roll attack
+
+	std::string damage_roll = creature[DAM_VAR];
+	
+}
+
+int * read_dam(std::string input)
+{
+	static int values[3];
+
+	int d = input.find("d");
+	int dice_length = input.length() - d + 1;
+
+	int plus = input.find(" + ") + 2;
+	int plus_length = input.length() + 1;
+	int mod = 0;
+
+	if (plus > 1) {
+		plus_length = input.length() - plus;
+		mod = stoi(input.substr(plus+1, input.length() - plus_length));
+	}
+
+	int num = stoi(input.substr(0,d));
+	int dice = stoi(input.substr(d+1, plus_length - dice_length));
+
+	values[0] = num;
+	values[1] = dice;
+	values[2] = mod;
+
+	return values;
+}
+
 int main()
 {
 	srand(time(NULL));	// Generate random seed.
@@ -40,14 +75,13 @@ int main()
 	string input;
 
 	getline(cin, input);
-	int deliminator = input.find("d");
-	int dice_length = input.length() - deliminator + 1;
 
-	int num = stoi(input.substr(0,deliminator));
-	int dice = stoi(input.substr(deliminator+1, dice_length));
+	int * dam_values = read_dam(input);
+
+	// cout << num << "d" << dice << " + " << mod << endl;
 
 	cout << "Attack: " << roll_dice(1,20,0) << endl;
-	cout << "Damage: " << roll_dice(num,dice,0) << endl;
+	cout << "Damage: " << roll_dice(dam_values[0],dam_values[1],dam_values[2]) << endl;
 
 	return 0;
 }
--- csv_reader.cpp
+++ csv_reader.cpp
@@ -13,35 +13,47 @@ using namespace std;
 // in vector of vector of strings.
 std::vector<std::vector<std::string> > CSV_Reader::getData()
 {
-    std::ifstream file(fileName);
-    std::vector<std::vector<std::string> > dataList;
-    std::string line = "";
-    // Iterate through each line and split the content using delimeter
-    while (getline(file, line))
-    {
-        std::vector<std::string> vec;
-        boost::algorithm::split(vec, line, boost::is_any_of(delimeter));
-        dataList.push_back(vec);
-    }
-    // Close the File
-    file.close();
-    return dataList;
+	std::ifstream file(fileName);
+	std::vector<std::vector<std::string> > dataList;
+	std::string line = "";
+	getline(file, line);
+	// Iterate through each line and split the content using delimeter
+	while (getline(file, line))
+	{
+        	std::vector<std::string> vec;
+        	boost::algorithm::split(vec, line, boost::is_any_of(delimeter));
+        	dataList.push_back(vec);
+    	}
+    	// Close the File
+    	file.close();
+    	return dataList;
+}
+
+std::vector<std::vector<std::string>> load_file(std::string filepath)
+{
+	// Create an object of CSV_Reader	
+	CSV_Reader reader(filepath);
+	// Get the data from the CSV file
+	std::vector<std::vector<std::string>> dataList = reader.getData();
+
+	return dataList;
 }
 
 int main()
 {
-    // Creating an object of CSVWriter
-    CSV_Reader reader("./stats/encounter1.csv");
-    // Get the data from CSV File
-    std::vector<std::vector<std::string> > dataList = reader.getData();
-    // Print the content of row by row on screen
-    for(std::vector<std::string> vec : dataList)
-    {
-        for(std::string data : vec)
-        {
-            std::cout<<data << " , ";
-        }
-        std::cout<<std::endl;
-    }
-    return 0;
+	std::string filepath = "./stats/encounter1.csv";
+
+	std::vector<std::vector<std::string>> dataList = load_file(filepath);
+
+    	// Print the content of row by row on screen
+    	for(std::vector<std::string> vec : dataList)
+    	{
+        	for(std::string data : vec)
+        	{
+        		std::cout<<data << " , ";
+        	}
+        	std::cout<<std::endl;
+    	}
+
+    	return 0;
 }
