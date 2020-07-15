21464a675b3150943008e77b57f98887657d62a4
MontyKhan <robclark.wgc@gmail.com>
2020-07-15 20:18:17 +0100

    Fixed incorrect random selection of target.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,25 @@
 
 using namespace std;
 
+/* brief: 	Roll initiative for all players and monsters involved, then have each perform an action
+	  	on their turn.
+	  	Currently only supports melee attacks against random opponents.
+   param: 	players - vector of players and monsters involved in encounter.
+   returns: 	nothing
+*/
 void run_encounter(std::vector <combatant> players)
 {
-	node * active_player = new node();
-	int i = 0;
+	node * active_player = new node();		// Create initialisation node for players.
+	int i = 0;					// Initialise counter to 0.
 
-	active_player = initiative_round(players);
+	active_player = initiative_round(players);	// Create circular list of players sorted by intiative. Point active_player at head.
 
-	cout << endl;
+	cout << endl;					// Add line break for readability.
 
-	int target_selector = (rand() % (players.size()+1)) - 1;
+	int potential_targets = players.size() - 1;
 
+	// Select target randomly. Value of 1 means next player in
+									// initiative order.
 
 	while (active_player->initiative < active_player->prev->initiative)
 	{
@@ -26,6 +34,8 @@ void run_encounter(std::vector <combatant> players)
 	{
 		node * target = active_player->next;
 
+		int target_selector = (rand() % potential_targets+1) - 1;
+
 		for (int i = 0; i < target_selector; i++)
 		{
 			target = target->next;
@@ -36,7 +46,7 @@ void run_encounter(std::vector <combatant> players)
 		if (result == dead)
 		{
 			remove_from_list(target);
-			target_selector--;
+			potential_targets--;
 		}
 
 		active_player = active_player->next;
@@ -45,6 +55,11 @@ void run_encounter(std::vector <combatant> players)
 	cout << active_player->player.getName() << " wins!" << endl;
 }
 
+/* brief: 	main function. Loads players and monsters from .csv file with hardcoded address.
+	  	Then prints each to screen, and runs an encounter with them.
+   param: 	none
+   returns: 	0
+*/
 int main() {
 	srand(time(NULL));	// Generate random seed.
 
