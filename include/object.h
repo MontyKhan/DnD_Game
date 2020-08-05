b1c7a96df15d432a894609da4b3d664421fcbc76
MontyKhan <robclark.wgc@gmail.com>
2020-08-05 17:11:43 +0100

    Issue #11 - Moved name property to parent.

--- include/object.h
+++ include/object.h
@@ -2,14 +2,20 @@
 #define OBJECT_H
 
 #include "pathfinding.h"
+#include <string>
 
 class object {
 protected:
+	std::string name;
 	location coordinates;
 public:
 	// Constructors
 	object() : coordinates(location(0,0,0)) {};
 	object(location Coordinates) : coordinates(Coordinates) {};
+
+	// Getters/setters
+	virtual std::string getName() { return name; };
+	virtual int setName(std::string Name) { name = Name; return 0; };
 };
 
 #endif
