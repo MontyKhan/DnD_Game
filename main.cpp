1de0d722cad2fab950b8e5723139eadab14706f7
MontyKhan <robclark.wgc@gmail.com>
2020-09-07 22:26:18 +0100

    Issue #13 - Added controllable player character.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <SFML/Graphics.hpp>
 #include "include/csv_reader.h"
 #include "include/combatant.h"
 #include "include/tools.h"
 #include "include/load_file.h"
 #include "include/monster.h"
 #include "include/tile.h"
+#include "include/display.h"
 
 using namespace std;
 
@@ -26,7 +26,7 @@ void run_encounter(std::vector <object*> players)
 	cout << endl;					// Add line break for readability.
 
 	// Graphics loop crudely inserted below, so declaring variables here for time being.
-	sf::RenderWindow window(sf::VideoMode(800,600), "DnD_Game");
+	sf::RenderWindow window(sf::VideoMode(WINDOW_W,WINDOW_H), "DnD_Game");
 	sf::Image background;
 
 	if (!(background.loadFromFile("./images/maps/river_bridge.jpg")))
@@ -38,11 +38,27 @@ void run_encounter(std::vector <object*> players)
 	sf::Sprite bg;
 	bg.setTexture(bg_texture);
 	bg.setScale(0.391,0.391);
+	std::vector<sf::Sprite> entities;
+	entities.push_back(bg);
+
+	sf::Image p_img;
+
+	if (!(p_img.loadFromFile("./images/sprites/player.png")))
+		std::cout << "Could not load from file!" << std::endl;
+
+	sf::Texture player_texture;
+	player_texture.loadFromImage(p_img);
+
+	sf::Sprite player;
+	player.setTexture(player_texture);
+	//bg.setScale(0.391,0.391);
+	entities.push_back(player);
 
 	cout << endl;
 
+	sf::Event event;
 	// Repeat until only one player is left.
-	while (active_player->next != active_player)
+	while (event.type != sf::Event::Closed)
 	{
 		// Make move and take attack
 		active_player->player->take_turn(active_player);
@@ -51,17 +67,23 @@ void run_encounter(std::vector <object*> players)
 		active_player = active_player->next;
 
 		// Going to stick graphics loop here temporarily, and rewrite everything when it's more developed.
-		sf::Event event;
+		
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::S))
+				entities[1].move(0.f, 5.f);
 		}
 
-		window.clear();
+		/*window.clear();
 		window.draw(bg);
-		window.display();
+		window.display();*/
+		updateScreen(&window,entities);
+
+		if (active_player->next == active_player)
+			break;
 	}
+
+	window.close();
 
 	// Debug code. State last fighter standing.
 	cout << active_player->player->getName() << " wins!" << endl;
